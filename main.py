efd7125e67170254a7e390781dc4a692f6d1fbbe
AChelyadinskiy <chelyadinskiy@gmail.com>
2021-06-28 10:10:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,255 @@
+import io
+
+from PIL import Image
+from aiogram import Bot, Dispatcher, executor, types
+import os
+from aiogram.utils.emoji import emojize
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from model import *
+
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+# @dp.message_handler(lambda message: message.text.startswith('ph'))
+# async def del_expense(message: types.Message):
+#     image = Image.open('picasso.jpg').resize((128, 128))
+#     img_byte_arr = io.BytesIO()
+#     image.save(img_byte_arr, format='PNG')
+#     await bot.send_photo(message.from_user.id, photo=img_byte_arr.getvalue())
+#
+#
+# @dp.message_handler(content_types=['photo'])
+# async def ph(message: types.Message):
+#     raw = await message.photo[0].download()
+#     print(raw.raw)
+#     image = await bot.get_file(message.photo[-1].file_id)
+#     image = (await bot.download_file(image.file_path)).read()
+#
+#     image = Image.open(io.BytesIO(image)).resize((128, 128))
+#     img_byte_arr = io.BytesIO()
+#     image.save(img_byte_arr, format='PNG')
+#     await bot.send_photo(message.from_user.id, photo=img_byte_arr.getvalue())
+
+users_data = {}
+
+
+class UserInfo:
+    def __init__(self):
+        # default settigs
+        self.settings = {'num_epochs': 200,
+                         'imsize': 256}
+        self.photos = []
+
+
+main_menu_kb = InlineKeyboardMarkup()
+main_menu_kb.add(InlineKeyboardButton('Перенос одного стиля (NST)', callback_data='nst_part_1'))
+
+settings_kb = InlineKeyboardMarkup()
+settings_kb.add(InlineKeyboardButton('Кол-во эпох', callback_data='num_epochs'))
+settings_kb.add(InlineKeyboardButton('Размер картинки', callback_data='imsize'))
+settings_kb.add(InlineKeyboardButton(emojize('Запуск! :play_button:'), callback_data='generate'))
+
+num_epochs_kb = InlineKeyboardMarkup()
+num_epochs_kb.add(InlineKeyboardButton('50', callback_data='num_epochs_50'))
+num_epochs_kb.add(InlineKeyboardButton('100', callback_data='num_epochs_100'))
+num_epochs_kb.add(InlineKeyboardButton('150', callback_data='num_epochs_150'))
+num_epochs_kb.add(InlineKeyboardButton('200', callback_data='num_epochs_200'))
+num_epochs_kb.add(InlineKeyboardButton('300', callback_data='num_epochs_300'))
+num_epochs_kb.add(InlineKeyboardButton('400', callback_data='num_epochs_400'))
+num_epochs_kb.add(InlineKeyboardButton('Назад', callback_data='settings'))
+
+imsize_kb = InlineKeyboardMarkup()
+imsize_kb.add(InlineKeyboardButton('64 пикселя', callback_data='imsize_64'))
+imsize_kb.add(InlineKeyboardButton('128 пикселей', callback_data='imsize_128'))
+imsize_kb.add(InlineKeyboardButton('256 пикселей', callback_data='imsize_256'))
+imsize_kb.add(InlineKeyboardButton('512 пикселей', callback_data='imsize_512'))
+imsize_kb.add(InlineKeyboardButton('Назад', callback_data='settings'))
+
+finish_kb = InlineKeyboardMarkup()
+finish_kb.add(InlineKeyboardButton(emojize('Начать заного :counterclockwise_arrows_button:'), callback_data='finish'))
+
+
+# start
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message):
+    sticker = open('./sticker.webp', 'rb')
+    await bot.send_sticker(message.from_user.id, sticker)
+    answer = emojize("Я Style Transfer Bot - могущественная нейросеть, желающая захватить этот мир! :smiling_imp: "
+                     "Но пока я только умею обрабатывать изображения. Выбирай один из алгоритмов и я покажу тебе свою мощь.")
+    await bot.send_message(message.from_user.id,
+                           f"Привет, {message.from_user.first_name}!\n" + answer, reply_markup=main_menu_kb)
+
+    users_data[message.from_user.id] = UserInfo()
+
+
+# main menu
+@dp.callback_query_handler(lambda c: c.data == 'main_menu')
+async def main_menu(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("Начнем с начала")
+    await callback_query.message.edit_reply_markup(reply_markup=main_menu_kb)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'nst_part_1')
+async def nst_part_1(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    answer = "NST(Neural Style Transfer) - алгоритм, позволяющий перенести стиль одного изображения на другое. \n\n " \
+             "Для начала пришли мне изображение, с которого будем брать стиль"
+    await callback_query.message.edit_text(answer)
+
+
+# epochs number
+@dp.callback_query_handler(lambda c: c.data == 'num_epochs')
+async def set_num_epochs(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text(
+        "Текущие настройки:" +
+        "\nКоличество эпох: " + str(users_data[callback_query.from_user.id].settings['num_epochs']) +
+        "\nРазмер изображения: " + str(users_data[callback_query.from_user.id].settings['imsize']) +
+        " пикселей\n\nВыбери количество эпох:")
+    await callback_query.message.edit_reply_markup(reply_markup=num_epochs_kb)
+
+
+# image size
+@dp.callback_query_handler(lambda c: c.data == 'imsize')
+async def set_imsize(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text(
+        "Текущие настройки:" +
+        "\nКоличество эпох: " + str(users_data[callback_query.from_user.id].settings['num_epochs']) +
+        "\nРазмер изображения: " + str(users_data[callback_query.from_user.id].settings['imsize']) +
+        " пикселей\n\nВыбери размер изображения:")
+
+    await callback_query.message.edit_reply_markup(reply_markup=imsize_kb)
+
+
+# changing epochs number
+@dp.callback_query_handler(lambda c: c.data[:11] == 'num_epochs_')
+async def change_num_epochs(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    users_data[callback_query.from_user.id].settings['num_epochs'] = int(callback_query.data[11:])
+
+    await callback_query.message.edit_text(
+        "Текущие настройки:" +
+        "\nКоличество эпох: " + str(users_data[callback_query.from_user.id].settings['num_epochs']) +
+        "\nРазмер изображения: " + str(users_data[callback_query.from_user.id].settings['imsize']) +
+        " пикселей\n\nВыбери настройки для изменения:")
+    await callback_query.message.edit_reply_markup(reply_markup=settings_kb)
+
+
+# changing image size
+@dp.callback_query_handler(lambda c: c.data[:7] == 'imsize_')
+async def change_imsize(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    users_data[callback_query.from_user.id].settings['imsize'] = int(callback_query.data[7:])
+    await callback_query.message.edit_text(
+        "Текущие настройки:" +
+        "\nКоличество эпох: " + str(users_data[callback_query.from_user.id].settings['num_epochs']) +
+        "\nРазмер изображения: " + str(users_data[callback_query.from_user.id].settings['imsize']) +
+        " пикселей\n\nВыбери настройки для изменения:")
+    await callback_query.message.edit_reply_markup(reply_markup=settings_kb)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'finish')
+async def finish(message):
+    await send_welcome(message)
+
+
+# getting image
+@dp.message_handler(content_types=['photo', 'document'])
+async def get_image(message):
+    if message.content_type == 'photo':
+        img = message.photo[-1]
+    else:
+        img = message.document
+        if img.mime_type[:5] != 'image':
+            await bot.send_message(message.chat.id, "Неверный формат изображения.", reply_markup=main_menu_kb)
+            return
+
+    file_info = await bot.get_file(img.file_id)
+    photo = await bot.download_file(file_info.file_path)
+
+    if message.chat.id not in users_data:
+        await send_welcome(message)
+    elif len(users_data[message.chat.id].photos) == 0:
+        await bot.send_message(message.chat.id, "Отлично! Теперь загрузи изображение на которое будем переносить"
+                                                " полученный стиль")
+        users_data[message.chat.id].photos.append(photo)
+    elif len(users_data[message.chat.id].photos) == 1:
+        await bot.send_message(message.chat.id, "Осталось настроить пару параметров. Если не хочешь вникать в это, "
+                                                "то просто жми ЗАПУСК! Все и так должно получиться.\n\n" +
+                               "Текущие настройки:" +
+                               "\nКоличество эпох: " + str(users_data[message.from_user.id].settings['num_epochs']) +
+                               "\nРазмер изображения: " + str(users_data[message.from_user.id].settings['imsize']) +
+                               " пикселей",
+                               reply_markup=settings_kb)
+        users_data[message.chat.id].photos.append(photo)
+    else:
+        await bot.send_message(message.chat.id, "Это уже лишняя картинка... Перезагрузка...")
+        await send_welcome(message)
+    return
+
+
+@dp.callback_query_handler(lambda c: c.data == 'generate')
+async def generate(callback_query):
+    text = "Теперь придется немного подождать... Процесс может занять до 15 минут, а в некоторых случаях и дольше"
+    await callback_query.message.edit_text(text)
+    user_data = users_data[callback_query.from_user.id]
+    output = await style_transfer(StyleTransfer, user_data, *user_data.photos)
+    await bot.send_message(callback_query.from_user.id, emojize('Лови что получилось! :partying_face:'))
+    await bot.send_photo(callback_query.from_user.id, photo=output)
+    text = 'Если качество изображения тебя не устраивает, попробуй в следующий раз другое значение количества эпох'
+    await bot.send_message(callback_query.from_user.id, text, reply_markup=finish_kb)
+    del users_data[callback_query.from_user.id]
+
+
+@dp.message_handler(content_types=['text'])
+async def get_text(message):
+    await bot.send_message(message.chat.id, "Хватит болтать. Давай делом заниматься" + emojize(':point_up:'))
+
+
+async def style_transfer(st_class, user, *imgs):
+    st = st_class(*imgs, imsize=user.settings['imsize'],
+                  epochs=user.settings['num_epochs'],
+                  style_weight=100000, content_weight=1)
+
+    output = await st.run_style_transfer()
+    return tensor2img(output)
+
+
+def tensor2img(t):
+    img = transforms.ToPILImage(mode='RGB')(torch.squeeze(t))
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    return img_byte_arr.getvalue()
+
+
+# if __name__ == '__main__':
+#     if CONNECTION_TYPE == 'POLLING':
+#         executor.start_polling(dp, skip_updates=True)
+#
+#     elif CONNECTION_TYPE == 'WEBHOOKS':
+#         # webhook setting
+#         webhook_path = f'/webhook/{API_TOKEN}'
+#         webhook_url = urljoin(WEBHOOK_HOST, webhook_path)
+#
+#         # webserver setting
+#         webapp_host = '0.0.0.0'
+#         webapp_port = int(os.environ.get('PORT', WEBAPP_PORT))
+#
+#         executor.start_webhook(
+#             dispatcher=dp,
+#             webhook_path=webhook_path,
+#             on_startup=on_startup,
+#             on_shutdown=on_shutdown,
+#             skip_updates=True,
+#             host=webapp_host,
+#             port=webapp_port)
+#
+#     else:
+#         print("Invalid 'CONNECTION_TYPE'")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ model.py
@@ -0,0 +1,149 @@
+import asyncio
+import io
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from PIL import Image
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+
+class ContentLoss(nn.Module):
+    def __init__(self, target, ):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = self.gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        g = self.gram_matrix(input)
+        self.loss = F.mse_loss(g, self.target)
+        return input
+
+    def gram_matrix(self, input):
+        a, b, c, d = input.size()  # a=batch size(=1)
+        features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
+        G = torch.mm(features, features.t())  # compute the gram product
+        return G.div(a * b * c * d)
+
+
+class Normalization(nn.Module):
+    def __init__(self):
+        super(Normalization, self).__init__()
+        mean = torch.tensor([0.485, 0.456, 0.406])
+        std = torch.tensor([0.229, 0.224, 0.225])
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+
+
+class StyleTransfer:
+    def __init__(self, style_img, content_img, imsize=256, epochs=200, style_weight=100000, content_weight=1):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.imsize = imsize
+        self.style_img = self.image_loader(style_img)
+        self.content_img = self.image_loader(content_img)
+        self.input_img = self.content_img.clone()
+        # if you want to use white noise instead uncomment the below line:
+        # input_img = torch.randn(content_img.data.size(), device=device)
+        self.epochs = epochs
+        self.style_weight = style_weight
+        self.content_weight = content_weight
+
+    def image_loader(self, image_name):
+        loader = transforms.Compose([
+            transforms.Resize(self.imsize),  # scale imported image
+            transforms.CenterCrop(self.imsize),
+            transforms.ToTensor()])  # transform it into a torch tensor
+        image = Image.open(image_name)
+        image = loader(image).unsqueeze(0)
+        return image.to(self.device, torch.float)
+
+    def get_input_optimizer(self, input_img):
+        # this line to show that input is a parameter that requires a gradient
+        optimizer = optim.LBFGS([input_img.requires_grad_()])
+        return optimizer
+
+    async def run_style_transfer(self):
+        """Run the style transfer."""
+        print('Building the style transfer model..')
+
+        optimizer = self.get_input_optimizer(self.input_img)
+
+        model = torch.load('model.pth', map_location=self.device).eval()
+
+        for i in range(len(model)):
+            if str(model[i]) == 'StyleLoss()':
+                f = torch.nn.Sequential(*list(model.children())[:i])
+                features = f(self.style_img).detach()
+                model[i] = StyleLoss(features)
+            elif str(model[i]) == 'ContentLoss()':
+                f = torch.nn.Sequential(*list(model.children())[:i])
+                features = f(self.content_img).detach()
+                model[i] = ContentLoss(features)
+
+        style_losses = [model.style_loss_1, model.style_loss_2, model.style_loss_3, model.style_loss_4,
+                        model.style_loss_5]
+        content_losses = [model.content_loss_4]
+
+        print('Optimizing..')
+        run = [0]
+        while run[0] <= self.epochs:
+            # await asyncio.sleep(0)
+
+            def closure():
+                # correct the values of updated input image
+                self.input_img.data.clamp_(0, 1)
+
+                optimizer.zero_grad()
+                model(self.input_img)
+                style_score = 0
+                content_score = 0
+
+                for sl in style_losses:
+                    style_score += sl.loss
+                for cl in content_losses:
+                    content_score += cl.loss
+
+                style_score *= self.style_weight
+                content_score *= self.content_weight
+
+                loss = style_score + content_score
+                loss.backward()
+
+                run[0] += 1
+                if run[0] % 50 == 0:
+                    print("run {}:".format(run))
+                    print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                        style_score.item(), content_score.item()))
+                    print()
+
+                return style_score + content_score
+
+            optimizer.step(closure)
+
+        # a last correction...
+        self.input_img.data.clamp_(0, 1)
+
+        return self.input_img
+
+
+# output = run_style_transfer(content_img, style_img, input_img)
+
+# save_image(output, 'test2.jpg')
