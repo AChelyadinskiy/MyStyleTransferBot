b1659f2c918f1daa387d81745a50745963252cf7
AChelyadinskiy <chelyadinskiy@gmail.com>
2021-06-28 20:14:18 +0200

    add webhook

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = 8443
+WEBAPP_PORT = 5000
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
